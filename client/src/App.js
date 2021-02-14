f2c150b4e963896f876ed1829412aa0597c2c3f5
ilaydaozdemir <71650829+ilaydaozdemir@users.noreply.github.com>
2021-02-14 19:45:00 +0300

    react router setup-1

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
-
+import './App.css';
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+    </Fragment>
+  </Router>
 );
 
 export default App;
